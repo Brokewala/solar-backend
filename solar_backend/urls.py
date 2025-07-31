767b03dfb5ceb88935f31f92dc03a2b07b841947
Brokewala <brokewala@gmail.com>
2025-07-31 14:28:27 +0300

    test url

--- solar_backend/urls.py
+++ solar_backend/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.static import static
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.conf import settings
 from django.contrib import admin
 from drf_yasg.views import get_schema_view
@@ -28,13 +28,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/solar/users/', include("users.urls")),
     path('api/solar/modules/', include("module.urls")),
-    path('api/solar/rating/', include("rating.urls")),
-    path('api/solar/battery/', include("battery.urls")),
-    path('api/solar/panneau/', include("panneau.urls")),
-    path('api/solar/prise/', include("prise.urls")),
-    path('api/solar/report/', include("report.urls")),
-    path('api/solar/subscription/', include("subscription.urls")),
-    path('api/solar/notification/', include("notification.urls")),
+    # path('api/solar/rating/', include("rating.urls")),
+    # path('api/solar/battery/', include("battery.urls")),
+    # path('api/solar/panneau/', include("panneau.urls")),
+    # path('api/solar/prise/', include("prise.urls")),
+    # path('api/solar/report/', include("report.urls")),
+    # path('api/solar/subscription/', include("subscription.urls")),
+    # path('api/solar/notification/', include("notification.urls")),
 
 ]
 

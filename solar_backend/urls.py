9a40adbd94b69116f1e2973675c8bf3d8c3d191a
Brokewala <brokewala@gmail.com>
2025-07-31 14:14:51 +0300

    swagger

--- solar_backend/urls.py
+++ solar_backend/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.contrib import admin
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="SolarNative API",
+      default_version='v1',
+      description="Documentation de l'API de SolarNative (battery, prise, panneau)",
+      contact=openapi.Contact(email="support@tonsite.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,3 +33,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]

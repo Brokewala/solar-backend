a41fe4795e1aff56b0418959067fb198319ea167
Brokewala <brokewala@gmail.com>
2025-07-31 14:20:26 +0300

    swagger

--- solar_backend/urls.py
+++ solar_backend/urls.py
@@ -8,16 +8,23 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="SolarNative API",
+      title="Snippets API",
       default_version='v1',
-      description="Documentation de l'API de SolarNative (battery, prise, panneau)",
-      contact=openapi.Contact(email="support@tonsite.com"),
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
 
+
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # 
     path('admin/', admin.site.urls),
     path('api/solar/users/', include("users.urls")),
     path('api/solar/modules/', include("module.urls")),
@@ -31,11 +38,7 @@ urlpatterns = [
 
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]

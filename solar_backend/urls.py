6075ccd47c4bf3bb99db5403e5e168c9f3e1fcf3
Brokewala <brokewala@gmail.com>
2025-07-31 14:39:27 +0300

    url

--- solar_backend/urls.py
+++ solar_backend/urls.py
@@ -28,13 +28,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/solar/users/', include("users.urls")),
     path('api/solar/modules/', include("module.urls")),
-    # path('api/solar/rating/', include("rating.urls")),
-    # path('api/solar/battery/', include("battery.urls")),
-    # path('api/solar/panneau/', include("panneau.urls")),
-    # path('api/solar/prise/', include("prise.urls")),
-    # path('api/solar/report/', include("report.urls")),
-    # path('api/solar/subscription/', include("subscription.urls")),
-    # path('api/solar/notification/', include("notification.urls")),
+    path('api/solar/rating/', include("rating.urls")),
+    path('api/solar/battery/', include("battery.urls")),
+    path('api/solar/panneau/', include("panneau.urls")),
+    path('api/solar/prise/', include("prise.urls")),
+    path('api/solar/report/', include("report.urls")),
+    path('api/solar/subscription/', include("subscription.urls")),
+    path('api/solar/notification/', include("notification.urls")),
 
 ]
 

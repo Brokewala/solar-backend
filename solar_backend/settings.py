05263a516dd6e57b41218d3cdf27afda1344da20
Brokewala <brokewala@gmail.com>
2025-09-03 19:43:12 +0300

    domane

--- solar_backend/settings.py
+++ solar_backend/settings.py
@@ -204,6 +204,7 @@ CORS_ALLOW_HEADERS = (
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://solar-backend-production-12aa.up.railway.app",
+    "https://solar-bakend-production.up.railway.app",
     "http://localhost:3000",
     "http://localhost:4200",
     "http://localhost:5173",

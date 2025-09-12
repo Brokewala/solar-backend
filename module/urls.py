d7a098241ab47f6a52d2c2b1fcf9af9bb350dda0
Brokewala <brokewala@gmail.com>
2025-09-12 13:50:09 +0300

    slash dans le url

--- module/urls.py
+++ module/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("all", views.get_all_module),
     path("create-module", views.create_module_all),
     path("modules/<str:user_id>/user", views.get_one_module_by_user),
-    path("modules/<str:user_id>/user-iot", views.get_one_module_by_user_for_IOT),
+    path("modules/<str:user_id>/user-iot/", views.get_one_module_by_user_for_IOT),
     path("modules/<str:reference>/reference", views.get_module_by_reference),
     path("modules", views.ModulesAPIView.as_view()),
     path("modules/<str:module_id>", views.ModulesAPIView.as_view()),

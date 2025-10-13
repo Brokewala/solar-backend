1a37351d20e2fde34cfdafc0853f789c28976cc5
JACQUES Jean Lodphin <96722110+Brokewala@users.noreply.github.com>
2025-10-13 20:47:29 +0300

    Add panneau monthly stats endpoint

--- stats/urls.py
+++ stats/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
         name="month-aggregate",
     ),
     path(
+        "panneau/<str:module_id>/<int:year>/<int:month>",
+        views.get_panneau_monthly_stats,
+        name="panneau-monthly-stats",
+    ),
+    path(
         "battery/<str:module_id>/<int:year>/<int:month>",
         views.get_battery_monthly_stats,
         name="battery-monthly-stats",

080e853d26ac10b14b334a47f9255be046b519d9
Brokewala <brokewala@gmail.com>
2025-12-05 08:13:39 +0300

    laster

--- graphique/routing.py
+++ graphique/routing.py
@@ -4,7 +4,6 @@ from .consumers import ModuleDataConsumer
 
 websocket_urlpatterns = [
 
-    # Données temps réel par module_id (Battery/Panneau/Prise du même module)
     re_path(
         r"ws/module/(?P<module_id>[-\w]+)/$",
         ModuleDataConsumer.as_asgi(),

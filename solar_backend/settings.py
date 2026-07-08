e3cdd6db75429f9704d379f0ccb99049997d769a
Brokewala <brokewala@gmail.com>
2026-07-08 11:10:08 +0200

    tls fals

--- solar_backend/settings.py
+++ solar_backend/settings.py
@@ -262,8 +262,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465
 EMAIL_HOST_USER = "rakotoarisoa.ga@gmail.com"
 EMAIL_HOST_PASSWORD = "loxb wora pney rane"
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 # ================
 # EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
